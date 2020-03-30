1b43ac73bd1f2ef7341535cc273020a29ccc41da
edrumm <esd0658@googlemail.com>
2020-03-30 17:42:19 +0100

    improved nav-bar dropdown, incomplete

--- js/drawer.js
+++ js/drawer.js
@@ -3,13 +3,29 @@ function clearDrawer() {
 }
 
 function toggleDrawer(reset = false) {
-    let navbar = document.getElementById("nav-list");
-    let links = navbar.getElementsByTagName("li");
+    let nav = document.getElementById("nav-list");
+    let list = document.querySelectorAll("#nav-list > li:not(#dropdown-btn)");
     let icon = document.getElementById("dropdown-btn").getElementsByTagName("a")[0];
 
     if (reset || icon.innerHTML === "▲") {
         icon.innerHTML = "☰";
+
+        // flex-direction
+        nav.style.flexDirection = "row";
+        icon.style.flexDirection = "row";
+
+        for (let item of list) {
+            item.style.display = "none";
+        }
+
     } else {
         icon.innerHTML = "▲";
+
+        nav.style.flexDirection = "column";
+        icon.style.flexDirection = "column";
+
+        for (let item of list) {
+            item.style.display = "block";
+        }
     }
 }
